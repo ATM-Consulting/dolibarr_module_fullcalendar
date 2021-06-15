950cb1cf52b0b874468adc8dd574a2d4ae104c22
atm-gregr <greg.rastklan@atm-consulting.fr>
2021-06-15 17:11:36 +0200

    fix retrocompatibility

--- js/fullcalendar.js.php
+++ js/fullcalendar.js.php
@@ -79,7 +79,14 @@ if(empty($refer) || preg_match('/comm\/action\/index.php/', $refer))
 		}
 	}
 
-	if (! empty($user->socid)) $sql.= " AND u.fk_soc = ".$user->socid;
+	if((float)DOL_VERSION <= 3.8)
+	{
+		if (! empty($user->societe_id)) $sql.= " AND u.fk_soc = ".$user->societe_id;
+	}
+	else
+	{
+		if (! empty($user->socid)) $sql.= " AND u.fk_soc = ".$user->socid;
+	}
 	if (! empty($conf->global->USER_HIDE_INACTIVE_IN_COMBOBOX) || $noactive) $sql.= " AND u.statut <> 0";
 
 	if(empty($conf->global->MAIN_FIRSTNAME_NAME_POSITION)){
