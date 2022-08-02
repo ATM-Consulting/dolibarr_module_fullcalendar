8dcbd5fea5757666b48ca1d81cd0e9bb6eb8f600
atm-greg <gregory.blemand@atm-consulting.fr>
2022-08-02 12:18:56 +0200

    version 2.4.8 and ChangeLog

--- core/modules/modfullcalendar.class.php
+++ core/modules/modfullcalendar.class.php
@@ -61,7 +61,7 @@ class modfullcalendar extends DolibarrModules
 		$this->description = "Description of module fullcalendar";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
-		$this->version = '2.4.7';
+		$this->version = '2.4.8';
 
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
