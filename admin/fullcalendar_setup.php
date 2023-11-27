3edaa9a0aa5b9c12e7b797f7773a91a2b73ce2b8
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2023-11-27 11:09:16 +0100

    Update admin/fullcalendar_setup.php
    Co-authored-by: John BOTELLA <john.botella@atm-consulting.fr>

--- admin/fullcalendar_setup.php
+++ admin/fullcalendar_setup.php
@@ -193,7 +193,7 @@ print '<td align="right" width="300">';
 print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">';
 print '<input type="hidden" name="token" value="'.$newToken.'">';
 print '<input type="hidden" name="action" value="set_FULLCALENDAR_HIDE_DAYS">';
-print '<input type="text" name="FULLCALENDAR_HIDE_DAYS" value="'.(getDolGlobalString('FULLCALENDAR_HIDE_DAYS') ?  getDolGlobalString('FULLCALENDAR_HIDE_DAYS') : '').'" />';
+print '<input type="text" name="FULLCALENDAR_HIDE_DAYS" value="'.getDolGlobalString('FULLCALENDAR_HIDE_DAYS') .'" />';
 print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
 print '</form>';
 print '</td></tr>';
